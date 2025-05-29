21c3ed068cbbf0e2acd0bcf95d8470766f94dc1e
Dcanosu <dcanosu@gmail.com>
2025-05-28 22:53:59 -0500

    DFS-22: Adding module rds to set up the bd for the app

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,20 @@
 output "status" {
-value = module.ec2_app_flask.status
+  value = module.ec2_app_flask.status
 }
 
 output "instance_id" {
-value = module.ec2_app_flask.instance_id
+  value = module.ec2_app_flask.instance_id
 }
 
 output "public_ip" {
-value = module.ec2_app_flask.public_ip
+  value = module.ec2_app_flask.public_ip
+}
+
+########## RDS ##########
+output "status_db" {
+  value = module.rds_db_app_flask.status
+}
+
+output "rds_endpoint" {
+  value = module.rds_db_app_flask.rds_endpoint
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -17,3 +17,25 @@ variable "volume_size" {
   description = "Size of the root EBS volume (in GiB)"
   type        = number
 }
+
+########## RDS ##########
+variable "db_instance_identifier" {
+  description = "The unique identifier for the RDS PostgreSQL instance. Must be lowercase and without spaces."
+  type        = string
+}
+
+variable "db_name" {
+  description = "The name of the database to create when the DB instance is created."
+  type    = string
+}
+
+variable "username" {
+  description = "Username for the master DB user."
+  type    = string
+}
+
+variable "password" {
+  description = "Password for the master DB user. Marked as sensitive for security."
+  type      = string
+  sensitive = true
+}
