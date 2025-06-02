5c54f66b4a850ee443aaa8f7f848f39ba6b366d2
@alejandroramirezlearning <danielramirez1022@hotmail.com>
2025-06-02 18:11:35 -0500

    comment the problematic admin name

--- infra/modules/rds/variables.tf
+++ infra/modules/rds/variables.tf
@@ -34,7 +34,7 @@ variable "instance_class" {
   default = "db.t3.micro"
 }
 
-variable "username" {
+//variable "username" {
   description = "Username for the master DB user."
   type    = string
   default = "admin"
@@ -44,7 +44,7 @@ variable "password" {
   description = "Password for the master DB user. Marked as sensitive for security."
   type      = string
   sensitive = true
-}
+}//
 
 # variable "parameter_group_name" {
 #   description = "The name of the DB parameter group to associate with this instance."
